30840e4057ad53e3a868676f8f8c1d1602ed86fa
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-05 23:00:26 +0000

    removed draw() and move() methods, modified constructor

--- src/main/java/com/ldts/donkeykong/model/game/dynamic/Hammer.java
+++ src/main/java/com/ldts/donkeykong/model/game/dynamic/Hammer.java
@@ -6,15 +6,4 @@ public class Hammer extends DynamicElement {
     public Hammer(Position p) {
         this.position = p;
     }
-
-    //TODO
-    @Override
-    public void draw() {
-
-    }
-
-    //TODO
-    public void move() {
-
-    }
 }
