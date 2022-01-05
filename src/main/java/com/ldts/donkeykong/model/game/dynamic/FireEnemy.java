0ea42de9291cf1175de00f4db5577f6dbfea54f2
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-05 23:06:16 +0000

    removed draw() and move() methods, modified constructor

--- src/main/java/com/ldts/donkeykong/model/game/dynamic/FireEnemy.java
+++ src/main/java/com/ldts/donkeykong/model/game/dynamic/FireEnemy.java
@@ -3,7 +3,7 @@ package com.ldts.donkeykong.model.game.dynamic;
 import com.ldts.donkeykong.model.base.Position;
 
 public class FireEnemy extends DynamicElement {
-    public FireEnemy(Position p) {
-        this.position = p;
+    public FireEnemy(Position position) {
+        super(position);
     }
 }
