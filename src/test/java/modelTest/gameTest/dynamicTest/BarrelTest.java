7cb21e3050f72062700cf9dd8407f4d866c4ce76
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-05 23:14:53 +0000

    tests for Barrel class done

---
+++ src/test/java/modelTest/gameTest/dynamicTest/BarrelTest.java
@@ -0,0 +1,31 @@
+package modelTest.gameTest.dynamicTest;
+
+import com.ldts.donkeykong.model.base.Position;
+import com.ldts.donkeykong.model.game.dynamic.Barrel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BarrelTest {
+    Barrel barrel, nonFieryBarrel;
+    Position p;
+
+    @BeforeEach
+    public void beforeEach() {
+        p = new Position(2,3);
+        barrel = new Barrel(p,true);
+        nonFieryBarrel = new Barrel(p, false);
+    }
+
+    @Test
+    public void constructorTest() {
+        Assertions.assertEquals(p.getX(), barrel.getPosition().getX());
+        Assertions.assertEquals(p.getY(), barrel.getPosition().getY());
+        Assertions.assertTrue(barrel.isFiery());
+    }
+
+    @Test
+    public void isFieryTest() {
+        Assertions.assertFalse(nonFieryBarrel.isFiery());
+    }
+}
