a096e776dc5a215d24b63d573d701a761a44d195
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-05 23:04:13 +0000

    added code for getUp(), getDown(), getLeft(), getRight() and equals() methods

--- src/main/java/com/ldts/donkeykong/model/base/Position.java
+++ src/main/java/com/ldts/donkeykong/model/base/Position.java
@@ -14,9 +14,7 @@ public class Position {
         this.x = x;
     }
 
-    public void setY(int y) {
-        this.y = y;
-    }
+    public void setY(int y) {this.y = y;}
 
     public int getX() {
         return x;
@@ -27,23 +25,27 @@ public class Position {
     }
 
     public Position getUp(){
-        return new Position(0,0);
+        return new Position(x - 1, y);
     }
 
     public Position getDown(){
-        return new Position(0,0);
+        return new Position(x + 1, y);
     }
 
     public Position getLeft(){
-        return new Position(0,0);
+        return new Position(x,y - 1);
     }
 
     public Position getRight(){
-        return new Position(0,0);
+        return new Position(x,y + 1);
     }
 
     @Override
     public boolean equals(Object o) {
-        return false;
+        if (this == o) return true;
+        if (o == null) return false;
+        if (getClass() != o.getClass()) return false;
+        Position p = (Position) o;
+        return x == p.getX() && y == p.getY();
     }
 }
