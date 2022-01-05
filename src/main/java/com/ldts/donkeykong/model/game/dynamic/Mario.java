6004142f8d37c10811435e5bc2b5cec1e45f6e6a
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-05 22:57:55 +0000

    removed draw() and move() methods, added setAsDead() method and added new constructor

--- src/main/java/com/ldts/donkeykong/model/game/dynamic/Mario.java
+++ src/main/java/com/ldts/donkeykong/model/game/dynamic/Mario.java
@@ -6,23 +6,16 @@ public class Mario extends DynamicElement {
     private boolean alive;
     private Position position;
 
-    public Mario(Position p) {
-        this.position = p;
+    public Mario(Position position) {
+        super(position);
+        this.alive = true;
     }
 
     public boolean isAlive() {
         return this.alive;
     }
 
-    //TODO
-    @Override
-    public void move() {
-
-    }
-
-    //TODO
-    @Override
-    public void draw() {
-
+    public void setAsDead() {
+        this.alive = false;
     }
 }
