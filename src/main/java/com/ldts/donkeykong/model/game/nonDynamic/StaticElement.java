d23523951b1457f5790b77186f58c8f7f39aefc4
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-05 22:37:30 +0000

    added constructor from superclass

--- src/main/java/com/ldts/donkeykong/model/game/nonDynamic/StaticElement.java
+++ src/main/java/com/ldts/donkeykong/model/game/nonDynamic/StaticElement.java
@@ -1,7 +1,10 @@
 package com.ldts.donkeykong.model.game.nonDynamic;
 
+import com.ldts.donkeykong.model.base.Position;
 import com.ldts.donkeykong.model.game.GameElement;
 
 public abstract class StaticElement extends GameElement {
-
+    public StaticElement(Position position) {
+        super(position);
+    }
 }
