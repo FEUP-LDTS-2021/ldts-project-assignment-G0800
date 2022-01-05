0a0aa544c31e1ddf7e7dad14bf1132aff559d92b
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-05 23:06:38 +0000

    removed move() method, added constructor

--- src/main/java/com/ldts/donkeykong/model/game/dynamic/DynamicElement.java
+++ src/main/java/com/ldts/donkeykong/model/game/dynamic/DynamicElement.java
@@ -1,7 +1,10 @@
 package com.ldts.donkeykong.model.game.dynamic;
 
+import com.ldts.donkeykong.model.base.Position;
 import com.ldts.donkeykong.model.game.GameElement;
 
 public abstract class DynamicElement extends GameElement {
-    public abstract void move();
+    public DynamicElement(Position position){
+        super(position);
+    }
 }
