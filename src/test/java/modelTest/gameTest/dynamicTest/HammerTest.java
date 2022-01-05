850f8c4ed0143ca317cb27c2dfd5ac8f78d8235e
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-05 23:16:25 +0000

    tests for Hammmer class done

---
+++ src/test/java/modelTest/gameTest/dynamicTest/HammerTest.java
@@ -0,0 +1,25 @@
+package modelTest.gameTest.dynamicTest;
+import com.ldts.donkeykong.model.base.Position;
+import com.ldts.donkeykong.model.game.dynamic.FireEnemy;
+import com.ldts.donkeykong.model.game.dynamic.Hammer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class HammerTest {
+
+    Position p;
+    Hammer hammer;
+
+    @BeforeEach
+    public void beforeEach() {
+        p = new Position(8,6);
+        hammer = new Hammer(p);
+    }
+
+    @Test
+    public void constructorTest() {
+        Assertions.assertEquals(p.getX(), hammer.getPosition().getX());
+        Assertions.assertEquals(p.getY(), hammer.getPosition().getY());
+    }
+}
