a6d8e267e4307afb5db17da9130acf0551a35d3a
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-05 23:07:26 +0000

    modified constructor

--- src/main/java/com/ldts/donkeykong/model/game/dynamic/Donkey.java
+++ src/main/java/com/ldts/donkeykong/model/game/dynamic/Donkey.java
@@ -3,7 +3,7 @@ package com.ldts.donkeykong.model.game.dynamic;
 import com.ldts.donkeykong.model.base.Position;
 
 public class Donkey extends DynamicElement {
-    public Donkey(Position p) {
-        this.position = p;
+    public Donkey(Position position) {
+        super (position);
     }
 }
