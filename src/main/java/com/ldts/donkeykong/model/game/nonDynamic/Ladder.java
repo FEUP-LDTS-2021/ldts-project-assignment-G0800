a9f1858616a52f6a888b94c29d47357a1453a1e0
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-06 00:44:23 +0000

    Ladder is now made of Stair objects (ArrayList)

--- src/main/java/com/ldts/donkeykong/model/game/nonDynamic/Ladder.java
+++ src/main/java/com/ldts/donkeykong/model/game/nonDynamic/Ladder.java
@@ -2,8 +2,28 @@ package com.ldts.donkeykong.model.game.nonDynamic;
 
 import com.ldts.donkeykong.model.base.Position;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ladder extends StaticElement {
-    public Ladder(Position position) {
-        super(position);
+    List<Stair> stairs;
+    public Ladder(Position startPosition, int height) {
+        super(startPosition);
+        stairs = new ArrayList<>();
+        createLadder(startPosition,height);
+    }
+
+    private void createLadder(Position position, int height) {
+        Stair stair = new Stair(position);
+        stairs.add(stair);
+        for (int index = 0; index < height-1; index++) {
+            Position newPosition = new Position(stair.getPosition().getUp());
+            stair = new Stair(newPosition);
+            stairs.add(stair);
+        }
+    }
+
+    public List<Stair> getStairs() {
+        return stairs;
     }
 }
