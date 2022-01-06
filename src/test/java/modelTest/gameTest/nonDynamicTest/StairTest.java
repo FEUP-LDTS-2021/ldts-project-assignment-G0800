650cd8dee46435c95494d20bd5b1594af3d4ce00
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-06 00:25:19 +0000

    tests for Stair class done

---
+++ src/test/java/modelTest/gameTest/nonDynamicTest/StairTest.java
@@ -0,0 +1,27 @@
+package modelTest.gameTest.nonDynamicTest;
+
+import com.ldts.donkeykong.model.base.Position;
+import com.ldts.donkeykong.model.game.nonDynamic.Stair;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class StairTest {
+
+    Stair stair;
+    Position p;
+
+    @BeforeEach
+    public void beforeEach() {
+        p = new Position(2,8);
+        stair = new Stair(p);
+    }
+
+    @Test
+    public void constructorTest() {
+        Assertions.assertEquals(p.getX(), stair.getPosition().getX());
+        Assertions.assertEquals(p.getY(), stair.getPosition().getY());
+
+    }
+}
