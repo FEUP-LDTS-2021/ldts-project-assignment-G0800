251f4861f1b727805f36f6773f9a28767cc9e60e
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-06 00:20:58 +0000

    implemented tests for Score class

---
+++ src/test/java/modelTest/gameTest/nonDynamicTest/ScoreTest.java
@@ -0,0 +1,36 @@
+package modelTest.gameTest.nonDynamicTest;
+
+import com.ldts.donkeykong.model.base.Position;
+import com.ldts.donkeykong.model.game.nonDynamic.Score;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ScoreTest {
+    Score score;
+    Position p;
+
+    @BeforeEach
+    public void beforeEach() {
+        p = new Position(0,20);
+        score = new Score(p);
+    }
+
+    @Test
+    public void constructorTest() {
+        Assertions.assertEquals(p.getX(), score.getPosition().getX());
+        Assertions.assertEquals(p.getY(), score.getPosition().getY());
+        Assertions.assertEquals(0, score.getScore());
+    }
+
+    @Test
+    public void setScoreTest(){
+        score.setScore(25);
+        Assertions.assertEquals(25, score.getScore());
+
+        score.setScore(2);
+        Assertions.assertEquals(2, score.getScore());
+    }
+
+
+}
