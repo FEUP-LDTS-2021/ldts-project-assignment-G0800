e1ecea6461f99cdd59f48f044ad37b0c3abd578a
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-06 00:25:01 +0000

    Class that defines a stair of a Ladder

---
+++ src/main/java/com/ldts/donkeykong/model/game/nonDynamic/Stair.java
@@ -0,0 +1,9 @@
+package com.ldts.donkeykong.model.game.nonDynamic;
+
+import com.ldts.donkeykong.model.base.Position;
+
+public class Stair extends StaticElement {
+    public Stair(Position position) {
+        super(position);
+    }
+}
