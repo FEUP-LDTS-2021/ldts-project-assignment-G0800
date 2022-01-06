3d322865812c81f13ccf653f8eeed5295a4ec5ec
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-06 00:20:21 +0000

    tests for OilBarrel class done

---
+++ src/test/java/modelTest/gameTest/nonDynamicTest/OilBarrelTest.java
@@ -0,0 +1,24 @@
+package modelTest.gameTest.nonDynamicTest;
+
+import com.ldts.donkeykong.model.base.Position;
+import com.ldts.donkeykong.model.game.nonDynamic.OilBarrel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class OilBarrelTest {
+    OilBarrel oilBarrel;
+    Position p;
+
+    @BeforeEach
+    public void beforeEach() {
+        p = new Position(2,4);
+        oilBarrel = new OilBarrel(p);
+    }
+
+    @Test
+    public void constructorTest() {
+        Assertions.assertEquals(p.getX(), oilBarrel.getPosition().getX());
+        Assertions.assertEquals(p.getY(), oilBarrel.getPosition().getY());
+    }
+}
