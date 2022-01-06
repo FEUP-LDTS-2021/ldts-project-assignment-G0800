f36fd0b985773efdd47ae508855d1d089cc46310
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-06 00:16:51 +0000

    corrected tests for ladder class

--- src/test/java/modelTest/gameTest/nonDynamicTest/LadderTest.java
+++ src/test/java/modelTest/gameTest/nonDynamicTest/LadderTest.java
@@ -11,18 +11,18 @@ public class LadderTest {
     public void constructorTest() {
         Ladder l = new Ladder(new Position(5,2),5);
         Assertions.assertEquals(l.getStairs().get(0).getPosition().getX(),5);
-        Assertions.assertEquals(l.getStairs().get(1).getPosition().getX(),6);
-        Assertions.assertEquals(l.getStairs().get(2).getPosition().getX(),7);
-        Assertions.assertEquals(l.getStairs().get(3).getPosition().getX(),8);
-        Assertions.assertEquals(l.getStairs().get(4).getPosition().getX(),9);
+        Assertions.assertEquals(l.getStairs().get(1).getPosition().getX(),4);
+        Assertions.assertEquals(l.getStairs().get(2).getPosition().getX(),3);
+        Assertions.assertEquals(l.getStairs().get(3).getPosition().getX(),2);
+        Assertions.assertEquals(l.getStairs().get(4).getPosition().getX(),1);
 
 
-        Ladder l2 = new Ladder(new Position(2,4),5);
-        Assertions.assertEquals(l2.getStairs().get(0).getPosition().getX(),2);
-        Assertions.assertEquals(l2.getStairs().get(1).getPosition().getX(),3);
-        Assertions.assertEquals(l2.getStairs().get(2).getPosition().getX(),4);
-        Assertions.assertEquals(l2.getStairs().get(3).getPosition().getX(),5);
+        Ladder l2 = new Ladder(new Position(10,4),5);
+        Assertions.assertEquals(l2.getStairs().get(0).getPosition().getX(),10);
+        Assertions.assertEquals(l2.getStairs().get(1).getPosition().getX(),9);
+        Assertions.assertEquals(l2.getStairs().get(2).getPosition().getX(),8);
+        Assertions.assertEquals(l2.getStairs().get(3).getPosition().getX(),7);
         Assertions.assertEquals(l2.getStairs().get(4).getPosition().getX(),6);
-        Assertions.assertEquals(l2.getStairs().get(0).getPosition().getY(),4);
+        Assertions.assertEquals(l2.getStairs().get(0).getPosition().getY(),5);
     }
 }
