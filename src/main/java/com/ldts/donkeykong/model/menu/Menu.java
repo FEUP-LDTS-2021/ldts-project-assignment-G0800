8bc5ac39b9aa91604f4ea44c5511380244c3d4b3
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-07 23:35:28 +0000

    Created Menu class

---
+++ src/main/java/com/ldts/donkeykong/model/menu/Menu.java
@@ -0,0 +1,45 @@
+package com.ldts.donkeykong.model.menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    private final List<String> entries;
+    private int currentEntry = 0;
+
+    public Menu() {
+        this.entries = null;
+    }
+
+    public void nextEntry() {
+
+    }
+
+    public void previousEntry() {
+
+    }
+
+    public String getEntry(int entry) {
+        return null;
+    }
+
+    public boolean isSelected(int entry) {
+        return false;
+    }
+
+    public boolean isSelectedStart() {
+        return false;
+    }
+
+    public boolean isSelectedInstructions() {
+        return false;
+    }
+
+    public boolean isSelectedExit() {
+        return false;
+    }
+
+    public int getNumberOfEntries() {
+        return -1;
+    }
+}
