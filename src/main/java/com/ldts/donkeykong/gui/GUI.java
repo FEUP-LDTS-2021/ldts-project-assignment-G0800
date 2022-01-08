4a036e30cab3ea79a1e26621e29e09ffe4c2b576
sofiamoura <57712018+sofiamoura@users.noreply.github.com>
2022-01-08 23:23:10 +0000

    GUI interface implemented

---
+++ src/main/java/com/ldts/donkeykong/gui/GUI.java
@@ -0,0 +1,35 @@
+package com.ldts.donkeykong.gui;
+
+import com.ldts.donkeykong.model.base.Position;
+
+import java.io.IOException;
+
+public interface GUI {
+    ACTION getNextAction() throws IOException;
+
+    void drawMario(Position position);
+
+    void drawDonkey(Position position);
+
+    void drawPrincess(Position position);
+
+    void drawBarrel(Position position, boolean isFiery);
+
+    void drawFireEnemy(Position position, int height);
+
+    void drawHammer(Position position);
+
+    void drawOilBarrel(Position position);
+
+    void drawStair(Position position, int dimension, char c);
+
+    void drawText(Position position, String text, String color);
+
+    void clear();
+
+    void refresh() throws IOException;
+
+    void close() throws IOException;
+
+    enum ACTION {UP, RIGHT, DOWN, LEFT, NONE, QUIT, SELECT}
+}
