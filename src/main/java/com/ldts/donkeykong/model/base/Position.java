32f9a1607435b99569fde42693a5c8244d0e2d3c
Milena Gouveia <milenaagouveia@gmail.com>
2022-01-08 20:25:43 +0000

    Fixed class Position

--- src/main/java/com/ldts/donkeykong/model/base/Position.java
+++ src/main/java/com/ldts/donkeykong/model/base/Position.java
@@ -30,19 +30,19 @@ public class Position {
     }
 
     public Position getUp(){
-        return new Position(x - 1, y);
+        return new Position(x, y - 1);
     }
 
     public Position getDown(){
-        return new Position(x + 1, y);
+        return new Position(x, y +1);
     }
 
     public Position getLeft(){
-        return new Position(x,y - 1);
+        return new Position(x - 1, y);
     }
 
     public Position getRight(){
-        return new Position(x,y + 1);
+        return new Position(x + 1, y);
     }
 
     @Override
