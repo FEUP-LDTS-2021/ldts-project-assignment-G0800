aaebaa96a16fab99bf1a14b9bbdb192fca4ce763
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-08 23:31:57 +0000

    added interface for methods and constructor

--- src/main/java/com/ldts/donkeykong/viewer/Viewer.java
+++ src/main/java/com/ldts/donkeykong/viewer/Viewer.java
@@ -1,5 +1,22 @@
 package com.ldts.donkeykong.viewer;
 
-public abstract class Viewer<T> {
+import com.ldts.donkeykong.gui.GUI;
 
+import java.io.IOException;
+
+public abstract class Viewer<T> {
+    private final T model;
+
+    public Viewer(T model) {
+        this.model = null;
+    }
+
+    public T getModel() {
+        return null;
+    }
+
+    public void draw(GUI gui) throws IOException {
+    }
+
+    protected abstract void drawElements(GUI gui);
 }
