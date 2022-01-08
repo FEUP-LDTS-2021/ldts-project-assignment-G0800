87f5282fa7505f6898abcd0abd2e8bb754842d8a
sofiamoura <57712018+sofiamoura@users.noreply.github.com>
2022-01-08 23:23:47 +0000

    LanternaGUI class implemented

---
+++ src/main/java/com/ldts/donkeykong/gui/LanternaGUI.java
@@ -0,0 +1,141 @@
+package com.ldts.donkeykong.gui;
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
+import com.ldts.donkeykong.model.base.Position;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class LanternaGUI implements GUI {
+    private final Screen screen;
+
+    public LanternaGUI(Screen screen) {
+        this.screen = screen;
+    }
+
+    public LanternaGUI(int width, int height) throws IOException, FontFormatException, URISyntaxException {
+        AWTTerminalFontConfiguration fontConfig = AWTTerminalFontConfiguration.getDefault();
+        Terminal terminal = createTerminal(width, height, fontConfig);
+        this.screen = createScreen(terminal);
+    }
+
+    private Screen createScreen(Terminal terminal) throws IOException {
+        final Screen screen;
+        screen = new TerminalScreen(terminal);
+
+        screen.setCursorPosition(null);
+        screen.startScreen();
+        screen.doResizeIfNecessary();
+        return screen;
+    }
+
+    private Terminal createTerminal(int width, int height, AWTTerminalFontConfiguration fontConfig) throws IOException {
+        TerminalSize terminalSize = new TerminalSize(width, height + 1);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory()
+                .setInitialTerminalSize(terminalSize);
+        terminalFactory.setForceAWTOverSwing(true);
+        terminalFactory.setTerminalEmulatorFontConfiguration(fontConfig);
+        Terminal terminal = terminalFactory.createTerminal();
+        return terminal;
+    }
+
+    public ACTION getNextAction() throws IOException {
+        KeyStroke keyStroke = screen.pollInput();
+        if (keyStroke == null) return ACTION.NONE;
+
+        if (keyStroke.getKeyType() == KeyType.EOF) return ACTION.QUIT;
+        if (keyStroke.getKeyType() == KeyType.Character && keyStroke.getCharacter() == 'q') return ACTION.QUIT;
+
+        if (keyStroke.getKeyType() == KeyType.ArrowUp) return ACTION.UP;
+        if (keyStroke.getKeyType() == KeyType.ArrowRight) return ACTION.RIGHT;
+        if (keyStroke.getKeyType() == KeyType.ArrowDown) return ACTION.DOWN;
+        if (keyStroke.getKeyType() == KeyType.ArrowLeft) return ACTION.LEFT;
+
+        if (keyStroke.getKeyType() == KeyType.Enter) return ACTION.SELECT;
+
+        return ACTION.NONE;
+    }
+
+    @Override
+    public void drawMario(Position position) {
+        drawCharacter(position.getX(), position.getY(), 'M', "#FF0000");
+    }
+
+    @Override
+    public void drawDonkey(Position position) {
+        drawCharacter(position.getX(), position.getY(), 'D', "#DF8958");
+    }
+
+    @Override
+    public void drawPrincess(Position position) {
+        drawCharacter(position.getX(), position.getY(), 'P', "#FFB7EB");
+    }
+
+    @Override
+    public void drawBarrel(Position position, boolean isFiery) {
+        String color;
+        color = isFiery ? "#D2A311" : "#0303D8";
+        drawCharacter(position.getX(), position.getY(), 'B', color);
+    }
+
+    @Override
+    public void drawFireEnemy(Position position, int height) {
+        drawCharacter(position.getX(), position.getY(), 'F', "#FFFF34");
+    }
+
+    @Override
+    public void drawHammer(Position position) {
+        drawCharacter(position.getX(), position.getY(), 'H', "");
+    }
+
+    @Override
+    public void drawOilBarrel(Position position) {
+        drawCharacter(position.getX(), position.getY(), 'O', "#2929ED");
+    }
+
+    @Override
+    public void drawStair(Position position, int dimension, char c) {
+        String color;
+        color = c == '-' ? "#8D1900" : "#17C4C4";
+        drawCharacter(position.getX(), position.getY(), c, color);
+    }
+
+    @Override
+    public void drawText(Position position, String text, String color) {
+        TextGraphics tg = screen.newTextGraphics();
+        tg.setForegroundColor(TextColor.Factory.fromString(color));
+        tg.putString(position.getX(), position.getY(), text);
+    }
+
+    private void drawCharacter(int x, int y, char c, String color) {
+        TextGraphics tg = screen.newTextGraphics();
+        tg.setForegroundColor(TextColor.Factory.fromString(color));
+        tg.putString(x, y, "" + c);
+    }
+
+    @Override
+    public void clear() {
+        screen.clear();
+    }
+
+    @Override
+    public void refresh() throws IOException {
+        screen.refresh();
+    }
+
+    @Override
+    public void close() throws IOException {
+        screen.close();
+    }
+
+}
