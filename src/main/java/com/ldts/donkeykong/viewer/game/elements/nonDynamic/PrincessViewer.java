e036f211600e2d6d6c578c93d1f721de454fc783
sofiamoura <57712018+sofiamoura@users.noreply.github.com>
2022-01-09 00:37:15 +0000

    PrincessViewer class implemented

--- src/main/java/com/ldts/donkeykong/viewer/game/elements/nonDynamic/PrincessViewer.java
+++ src/main/java/com/ldts/donkeykong/viewer/game/elements/nonDynamic/PrincessViewer.java
@@ -1,11 +1,12 @@
 package com.ldts.donkeykong.viewer.game.elements.nonDynamic;
 
 import com.ldts.donkeykong.gui.GUI;
+import com.ldts.donkeykong.model.game.elements.nonDynamic.Princess;
 import com.ldts.donkeykong.model.game.elements.nonDynamic.StaticElement;
 
 public class PrincessViewer extends StaticElementViewer {
-    @Override
     public void draw(StaticElement element, GUI gui) {
-
+        Princess princess = (Princess) element;
+        gui.drawPrincess(princess.getPosition());
     }
 }
