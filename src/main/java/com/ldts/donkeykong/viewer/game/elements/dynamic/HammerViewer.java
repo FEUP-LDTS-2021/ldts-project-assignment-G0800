84e5aab595d2784696cd7e755bef68cb1c2b4194
Milena Gouveia <milenaagouveia@gmail.com>
2022-01-09 00:44:40 +0000

    MarioViewer implementation

--- src/main/java/com/ldts/donkeykong/viewer/game/elements/dynamic/HammerViewer.java
+++ src/main/java/com/ldts/donkeykong/viewer/game/elements/dynamic/HammerViewer.java
@@ -2,10 +2,12 @@ package com.ldts.donkeykong.viewer.game.elements.dynamic;
 
 import com.ldts.donkeykong.gui.GUI;
 import com.ldts.donkeykong.model.game.elements.dynamic.DynamicElement;
+import com.ldts.donkeykong.model.game.elements.dynamic.Hammer;
 
 public class HammerViewer extends DynamicElementViewer{
     @Override
     public void draw(DynamicElement element, GUI gui) {
-
+        Hammer hammer = (Hammer) element;
+        gui.drawHammer(hammer.getPosition());
     }
 }
--- src/main/java/com/ldts/donkeykong/viewer/game/elements/dynamic/MarioViewer.java
+++ src/main/java/com/ldts/donkeykong/viewer/game/elements/dynamic/MarioViewer.java
@@ -2,10 +2,12 @@ package com.ldts.donkeykong.viewer.game.elements.dynamic;
 
 import com.ldts.donkeykong.gui.GUI;
 import com.ldts.donkeykong.model.game.elements.dynamic.DynamicElement;
+import com.ldts.donkeykong.model.game.elements.dynamic.Mario;
 
 public class MarioViewer extends DynamicElementViewer {
     @Override
     public void draw(DynamicElement element, GUI gui) {
-
+        Mario mario= (Mario) element;
+        gui.drawMario(mario.getPosition());
     }
 }
