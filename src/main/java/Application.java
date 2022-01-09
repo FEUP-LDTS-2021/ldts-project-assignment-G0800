4bae0a4968c72f538f09e22af4e99605a201e93a
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-09 09:47:39 +0000

    added code to main() function just to print the ArenaViewer

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,26 @@
+import com.ldts.donkeykong.gui.LanternaGUI;
+import com.ldts.donkeykong.model.game.arena.Arena;
+import com.ldts.donkeykong.model.game.arena.ArenaBuilderLoader;
+import com.ldts.donkeykong.viewer.game.arena.ArenaViewer;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("This branch is for active development!");
+        try {
+            LanternaGUI gui = new LanternaGUI(20,20);
+            Arena arena = new ArenaBuilderLoader(1).createArena();
+            ArenaViewer view = new ArenaViewer(arena);
+            view.draw(gui);
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (FontFormatException e) {
+            e.printStackTrace();
+        }
+
     }
 }
