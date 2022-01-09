26947d3a457b0d980938ec111ca5601780f52b6e
Milena Gouveia <milenaagouveia@gmail.com>
2022-01-09 00:41:34 +0000

    BarrelViewer Implementation

--- src/main/java/com/ldts/donkeykong/viewer/game/elements/dynamic/BarrelViewer.java
+++ src/main/java/com/ldts/donkeykong/viewer/game/elements/dynamic/BarrelViewer.java
@@ -1,11 +1,13 @@
 package com.ldts.donkeykong.viewer.game.elements.dynamic;
 
 import com.ldts.donkeykong.gui.GUI;
+import com.ldts.donkeykong.model.game.elements.dynamic.Barrel;
 import com.ldts.donkeykong.model.game.elements.dynamic.DynamicElement;
 
 public class BarrelViewer extends DynamicElementViewer {
     @Override
     public void draw(DynamicElement element, GUI gui) {
-
+        Barrel barrel= (Barrel) element;
+        gui.drawBarrel(barrel.getPosition(), barrel.isFiery());
     }
 }
