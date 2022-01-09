a70670bc1fd3429ebf4f059580eddcbad0c522cc
Milena Gouveia <milenaagouveia@gmail.com>
2022-01-09 00:43:05 +0000

    FireEnemyViewer implementations

--- src/main/java/com/ldts/donkeykong/viewer/game/elements/dynamic/FireEnemyViewer.java
+++ src/main/java/com/ldts/donkeykong/viewer/game/elements/dynamic/FireEnemyViewer.java
@@ -2,10 +2,12 @@ package com.ldts.donkeykong.viewer.game.elements.dynamic;
 
 import com.ldts.donkeykong.gui.GUI;
 import com.ldts.donkeykong.model.game.elements.dynamic.DynamicElement;
+import com.ldts.donkeykong.model.game.elements.dynamic.FireEnemy;
 
 public class FireEnemyViewer extends DynamicElementViewer {
     @Override
     public void draw(DynamicElement element, GUI gui) {
-
+        FireEnemy fireEnemy = (FireEnemy) element;
+        gui.drawFireEnemy(fireEnemy.getPosition());
     }
 }
