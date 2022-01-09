43a81e41769e804b2cfa1355bc49e1912148a889
Milena Gouveia <milenaagouveia@gmail.com>
2022-01-09 00:42:18 +0000

    DonkeyViewer implementation

--- src/main/java/com/ldts/donkeykong/viewer/game/elements/dynamic/DonkeyViewer.java
+++ src/main/java/com/ldts/donkeykong/viewer/game/elements/dynamic/DonkeyViewer.java
@@ -1,11 +1,13 @@
 package com.ldts.donkeykong.viewer.game.elements.dynamic;
 
 import com.ldts.donkeykong.gui.GUI;
+import com.ldts.donkeykong.model.game.elements.dynamic.Donkey;
 import com.ldts.donkeykong.model.game.elements.dynamic.DynamicElement;
 
 public class DonkeyViewer extends DynamicElementViewer {
     @Override
     public void draw(DynamicElement element, GUI gui) {
-
+        Donkey donkey= (Donkey) element;
+        gui.drawDonkey(donkey.getPosition());
     }
 }
