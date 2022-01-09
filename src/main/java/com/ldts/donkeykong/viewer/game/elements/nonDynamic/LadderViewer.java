43df8c49d72ec8fb727b8ee0a6e3c3b11e605259
sofiamoura <57712018+sofiamoura@users.noreply.github.com>
2022-01-09 00:35:29 +0000

    LadderViewer class implemented

--- src/main/java/com/ldts/donkeykong/viewer/game/elements/nonDynamic/LadderViewer.java
+++ src/main/java/com/ldts/donkeykong/viewer/game/elements/nonDynamic/LadderViewer.java
@@ -1,11 +1,16 @@
 package com.ldts.donkeykong.viewer.game.elements.nonDynamic;
 
 import com.ldts.donkeykong.gui.GUI;
+import com.ldts.donkeykong.model.game.elements.nonDynamic.Ladder;
+import com.ldts.donkeykong.model.game.elements.nonDynamic.Stair;
 import com.ldts.donkeykong.model.game.elements.nonDynamic.StaticElement;
 
 public class LadderViewer extends StaticElementViewer {
     @Override
     public void draw(StaticElement element, GUI gui) {
-
+        Ladder ladder = (Ladder) element;
+        for (Stair stair: ladder.getStairs()) {
+            gui.drawStair(element.getPosition(), '|');
+        }
     }
 }
