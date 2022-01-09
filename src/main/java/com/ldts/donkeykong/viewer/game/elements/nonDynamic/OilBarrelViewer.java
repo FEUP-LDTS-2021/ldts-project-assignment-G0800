d55ab8ba3bf5cecabe01fd8b83f94363a8678280
sofiamoura <57712018+sofiamoura@users.noreply.github.com>
2022-01-09 00:36:33 +0000

    OilBarrelViewer class implemented

--- src/main/java/com/ldts/donkeykong/viewer/game/elements/nonDynamic/OilBarrelViewer.java
+++ src/main/java/com/ldts/donkeykong/viewer/game/elements/nonDynamic/OilBarrelViewer.java
@@ -1,11 +1,13 @@
 package com.ldts.donkeykong.viewer.game.elements.nonDynamic;
 
 import com.ldts.donkeykong.gui.GUI;
+import com.ldts.donkeykong.model.game.elements.nonDynamic.OilBarrel;
 import com.ldts.donkeykong.model.game.elements.nonDynamic.StaticElement;
 
 public class OilBarrelViewer extends StaticElementViewer {
     @Override
     public void draw(StaticElement element, GUI gui) {
-
+        OilBarrel oilBarrel = (OilBarrel) element;
+        gui.drawOilBarrel(oilBarrel.getPosition());
     }
 }
