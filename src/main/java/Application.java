b93412a7a9cdf90ce483780d5a31c65fd61b3e36
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-13 21:46:04 +0000

    Called MenuViewer in main function

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,4 @@
 import com.ldts.donkeykong.gui.LanternaGUI;
-import com.ldts.donkeykong.model.game.arena.Arena;
-import com.ldts.donkeykong.model.game.arena.ArenaBuilderLoader;
-import com.ldts.donkeykong.viewer.game.arena.ArenaViewer;
 import com.ldts.donkeykong.viewer.menu.MenuViewer;
 import com.ldts.donkeykong.model.menu.Menu;
 
@@ -13,8 +10,6 @@ public class Application {
     public static void main(String[] args) {
         try {
             LanternaGUI gui = new LanternaGUI(20,20);
-            //Arena arena = new ArenaBuilderLoader(1).createArena();
-            //ArenaViewer view = new ArenaViewer(arena);
             Menu menu = new Menu();
             MenuViewer menuViewer = new MenuViewer(menu);
             menuViewer.draw(gui);
