1845840903a94f098a1ef5984f7dbb5ca9af1853
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-13 21:45:30 +0000

    Called MenuViewer in main function

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,8 @@ import com.ldts.donkeykong.gui.LanternaGUI;
 import com.ldts.donkeykong.model.game.arena.Arena;
 import com.ldts.donkeykong.model.game.arena.ArenaBuilderLoader;
 import com.ldts.donkeykong.viewer.game.arena.ArenaViewer;
+import com.ldts.donkeykong.viewer.menu.MenuViewer;
+import com.ldts.donkeykong.model.menu.Menu;
 
 import java.awt.*;
 import java.io.IOException;
@@ -11,9 +13,11 @@ public class Application {
     public static void main(String[] args) {
         try {
             LanternaGUI gui = new LanternaGUI(20,20);
-            Arena arena = new ArenaBuilderLoader(1).createArena();
-            ArenaViewer view = new ArenaViewer(arena);
-            view.draw(gui);
+            //Arena arena = new ArenaBuilderLoader(1).createArena();
+            //ArenaViewer view = new ArenaViewer(arena);
+            Menu menu = new Menu();
+            MenuViewer menuViewer = new MenuViewer(menu);
+            menuViewer.draw(gui);
         } catch (IOException i) {
             i.printStackTrace();
         } catch (URISyntaxException e) {
