3d789ca90cc6d9c59b89978ea9cdf22aacb00c71
Milena Gouveia <milenaagouveia@gmail.com>
2022-01-14 22:58:35 +0000

    Created MarioController class

---
+++ src/main/java/com/ldts/donkeykong/controller/game/elements/dynamic/MarioController.java
@@ -0,0 +1,38 @@
+package com.ldts.donkeykong.controller.game.elements.dynamic;
+
+import com.ldts.donkeykong.model.base.Position;
+import com.ldts.donkeykong.model.game.elements.dynamic.Barrel;
+
+public class MarioController{
+    public void moveUp(){
+
+    }
+
+    public void moveDown(){
+
+    }
+
+    public void moveLeft(){
+
+    }
+
+    public void moveRight(){
+
+    }
+
+    public void moveMario(Position position) {
+
+    }
+
+    public boolean hasHammer(){
+        return false;
+    }
+
+    public boolean reachedPrincess(){
+        return false;
+    }
+
+    public boolean touchedBarrel(Barrel barrel){
+        return false;
+    }
+}
