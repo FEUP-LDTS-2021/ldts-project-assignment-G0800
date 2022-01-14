4420830cfcfec137ed838a90c4f4e35d062f379c
Milena Gouveia <milenaagouveia@gmail.com>
2022-01-14 22:57:51 +0000

    Created FireEnemyController

---
+++ src/main/java/com/ldts/donkeykong/controller/game/elements/dynamic/FireEnemyController.java
@@ -0,0 +1,14 @@
+package com.ldts.donkeykong.controller.game.elements.dynamic;
+
+import com.ldts.donkeykong.model.game.elements.dynamic.FireEnemy;
+
+public class FireEnemyController {
+
+    public boolean touchedMario(){
+        return false;
+    }
+
+    public void moveFireEnemy(FireEnemy fireEnemy){
+
+    }
+}
