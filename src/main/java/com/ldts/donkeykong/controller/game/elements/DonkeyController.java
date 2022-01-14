96121cea8c4b57e3c04699944f73035a3c2d49c2
sofiamoura <57712018+sofiamoura@users.noreply.github.com>
2022-01-14 22:25:34 +0000

    DonkeyController class created

---
+++ src/main/java/com/ldts/donkeykong/controller/game/elements/DonkeyController.java
@@ -0,0 +1,24 @@
+package com.ldts.donkeykong.controller.game.elements;
+
+import com.ldts.donkeykong.Application;
+import com.ldts.donkeykong.gui.GUI;
+import com.ldts.donkeykong.model.base.Position;
+import com.ldts.donkeykong.model.game.arena.Arena;
+import com.ldts.donkeykong.model.game.elements.dynamic.Barrel;
+import com.ldts.donkeykong.model.game.elements.dynamic.Donkey;
+
+import java.io.IOException;
+
+public class DonkeyController {
+
+    public DonkeyController(Arena arena) {
+
+    }
+
+    public void throwBarrel(Donkey donkey, Barrel barrel) {
+
+    }
+
+    public void move(Donkey donkey, Position position) {
+    }
+}
