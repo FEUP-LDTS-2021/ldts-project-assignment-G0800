34d200171552deb977921540c59b4e1c3e073824
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-15 11:44:12 +0000

    Created OilBarrel controller class

---
+++ src/main/java/com/ldts/donkeykong/controller/game/elements/nonDynamic/OilBarrelController.java
@@ -0,0 +1,15 @@
+package com.ldts.donkeykong.controller.game.elements.nonDynamic;
+
+import com.ldts.donkeykong.model.game.arena.Arena;
+import com.ldts.donkeykong.model.game.elements.dynamic.Barrel;
+
+public class OilBarrelController  {
+
+    public OilBarrelController(Arena arena) {
+
+    }
+
+    public boolean hasFieryBarrel(Barrel barrel) {
+        return false;
+    }
+}
