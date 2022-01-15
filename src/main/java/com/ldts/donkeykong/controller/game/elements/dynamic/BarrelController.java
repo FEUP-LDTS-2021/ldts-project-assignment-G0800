4deea1f4b2b2d9a7dc91202f180debdccbcc4841
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-15 14:23:50 +0000

    Created Barrel controller class

---
+++ src/main/java/com/ldts/donkeykong/controller/game/elements/dynamic/BarrelController.java
@@ -0,0 +1,17 @@
+package com.ldts.donkeykong.controller.game.elements.dynamic;
+
+import com.ldts.donkeykong.model.base.Position;
+import com.ldts.donkeykong.model.game.arena.Arena;
+import com.ldts.donkeykong.model.game.elements.dynamic.Barrel;
+
+public class BarrelController {
+
+    public BarrelController(Arena arena) {
+
+    }
+
+    private void moveBarrel(Barrel barrel) {
+
+    }
+
+}
