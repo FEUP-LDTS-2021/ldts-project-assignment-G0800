d305834aa4df5e7d042ba3ae577946940f11bff8
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-15 11:44:18 +0000

    Created Hammer controller class

---
+++ src/main/java/com/ldts/donkeykong/controller/game/elements/dynamic/HammerController.java
@@ -0,0 +1,19 @@
+package com.ldts.donkeykong.controller.game.elements.dynamic;
+
+import com.ldts.donkeykong.model.base.Position;
+import com.ldts.donkeykong.model.game.arena.Arena;
+import com.ldts.donkeykong.model.game.elements.dynamic.Barrel;
+
+public class HammerController {
+    public HammerController(Arena arena) {
+
+    }
+
+    public boolean touchedBarrel(Barrel barrel) {
+        return false;
+    }
+
+    public void moveHammer(Position position) {
+
+    }
+}
