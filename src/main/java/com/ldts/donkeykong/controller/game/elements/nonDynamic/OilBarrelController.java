cd48886a99ec957dda95d4f376cfed76dd94359d
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-16 21:19:41 +0000

    OilBarrelController now extends from GameController

--- src/main/java/com/ldts/donkeykong/controller/game/elements/nonDynamic/OilBarrelController.java
+++ src/main/java/com/ldts/donkeykong/controller/game/elements/nonDynamic/OilBarrelController.java
@@ -1,15 +1,25 @@
 package com.ldts.donkeykong.controller.game.elements.nonDynamic;
 
+import com.ldts.donkeykong.Application;
+import com.ldts.donkeykong.controller.game.GameController;
+import com.ldts.donkeykong.gui.GUI;
 import com.ldts.donkeykong.model.game.arena.Arena;
 import com.ldts.donkeykong.model.game.elements.dynamic.Barrel;
 
-public class OilBarrelController  {
+import java.io.IOException;
+
+public class OilBarrelController extends GameController {
 
     public OilBarrelController(Arena arena) {
-
+        super(arena);
     }
 
     public boolean hasFieryBarrel(Barrel barrel) {
         return false;
+    }
+
+    @Override
+    public void step(Application application, GUI.ACTION action, long time) throws IOException {
+
     }
 }
