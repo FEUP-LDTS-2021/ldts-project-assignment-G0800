4772a39ff8e7294eed59b37dac1b0737acb64233
Milena Gouveia <milenaagouveia@gmail.com>
2022-01-16 11:26:09 +0000

    Added lastMove field and changed some methods

--- src/main/java/com/ldts/donkeykong/controller/game/elements/dynamic/FireEnemyController.java
+++ src/main/java/com/ldts/donkeykong/controller/game/elements/dynamic/FireEnemyController.java
@@ -1,14 +1,32 @@
 package com.ldts.donkeykong.controller.game.elements.dynamic;
 
+import com.ldts.donkeykong.Application;
+import com.ldts.donkeykong.controller.Controller;
+import com.ldts.donkeykong.gui.GUI;
+import com.ldts.donkeykong.model.base.Position;
+import com.ldts.donkeykong.model.game.arena.Arena;
 import com.ldts.donkeykong.model.game.elements.dynamic.FireEnemy;
 
-public class FireEnemyController {
+import java.io.IOException;
 
-    public boolean touchedMario(){
+public class FireEnemyController extends Controller<Arena> {
+    private long lastMove;
+
+    public FireEnemyController(Arena arena){
+        super(arena);
+
+    }
+
+    @Override
+    public void step(Application application, GUI.ACTION action, long time) throws IOException {
+
+    }
+
+    public boolean touchedMario(FireEnemy fireEnemy){
         return false;
     }
 
-    public void moveFireEnemy(FireEnemy fireEnemy){
+    public void moveFireEnemy(FireEnemy fireEnemy, Position position){
 
     }
 }
