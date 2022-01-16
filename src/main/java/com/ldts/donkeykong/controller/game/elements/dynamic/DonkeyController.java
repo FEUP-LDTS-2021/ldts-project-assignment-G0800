f8f129e1ab190917fbafece5b64303c7d78e492b
sofiamoura <57712018+sofiamoura@users.noreply.github.com>
2022-01-16 21:58:36 +0000

    DonkeyController now extends from GameController

--- src/main/java/com/ldts/donkeykong/controller/game/elements/dynamic/DonkeyController.java
+++ src/main/java/com/ldts/donkeykong/controller/game/elements/dynamic/DonkeyController.java
@@ -1,6 +1,7 @@
 package com.ldts.donkeykong.controller.game.elements.dynamic;
 
 import com.ldts.donkeykong.Application;
+import com.ldts.donkeykong.controller.Controller;
 import com.ldts.donkeykong.gui.GUI;
 import com.ldts.donkeykong.model.base.Position;
 import com.ldts.donkeykong.model.game.arena.Arena;
@@ -9,10 +10,10 @@ import com.ldts.donkeykong.model.game.elements.dynamic.Donkey;
 
 import java.io.IOException;
 
-public class DonkeyController {
+public class DonkeyController extends Controller<Arena> {
 
     public DonkeyController(Arena arena) {
-
+        super(arena);
     }
 
     public void throwBarrel(Barrel barrel) {
@@ -20,5 +21,10 @@ public class DonkeyController {
     }
 
     public void move(Position position) {
+    }
+
+    @Override
+    public void step(Application application, GUI.ACTION action, long time) throws IOException {
+
     }
 }
