3315a2681f64481620e0c7f5f29a2abb146e2b92
Milena Gouveia <milenaagouveia@gmail.com>
2022-01-17 19:22:55 +0000

    Implementation of FireEnemyController

--- src/main/java/com/ldts/donkeykong/controller/game/elements/dynamic/FireEnemyController.java
+++ src/main/java/com/ldts/donkeykong/controller/game/elements/dynamic/FireEnemyController.java
@@ -12,19 +12,43 @@ public class FireEnemyController extends Controller<Arena> {
 
     public FireEnemyController(Arena arena){
         super(arena);
-
+        lastMove = 0;
     }
 
     @Override
     public void step(Application application, GUI.ACTION action, long time){
+        if (time - lastMove > 500){
 
+            for (FireEnemy f: getModel().getFireEnemies()){
+                Position enemyPosition = f.getPosition();
+                Position marioPosition = getModel().getMario().getPosition();
+                Position nextPosition;
+
+                if (enemyPosition.getY() == marioPosition.getY()){
+                    if (enemyPosition.getX() < marioPosition.getX())
+                        nextPosition = getModel().isStructure(enemyPosition.getRight().getDown()) ? enemyPosition.getRight() : enemyPosition.getLeft();
+                    else
+                        nextPosition = getModel().isStructure(enemyPosition.getLeft().getDown()) ? enemyPosition.getLeft() : enemyPosition.getRight();
+                }
+
+                else
+                    nextPosition = (int) (Math.random() * 2) == 1 ? enemyPosition.getRight() : enemyPosition.getLeft();
+
+                moveFireEnemy(f, nextPosition);
+            }
+            this.lastMove = time;
+        }
     }
 
     public boolean touchedMario(FireEnemy fireEnemy){
-        return false;
+        return fireEnemy.getPosition().equals(getModel().getMario().getPosition());
     }
 
     public void moveFireEnemy(FireEnemy fireEnemy, Position position){
-
+        if (getModel().isEmpty(position) && this.getModel().isStructure(position.getDown())){
+            fireEnemy.setPosition(position);
+            if (touchedMario(fireEnemy))
+                getModel().getMario().setAsDead();
+        }
     }
 }
