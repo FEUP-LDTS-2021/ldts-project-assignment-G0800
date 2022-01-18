7679a028b49d2fdb78f042313fac3122f4fe9caf
sofiamoura <57712018+sofiamoura@users.noreply.github.com>
2022-01-18 09:46:01 +0000

    Small change due to states creation

--- src/main/java/com/ldts/donkeykong/controller/game/arena/ArenaController.java
+++ src/main/java/com/ldts/donkeykong/controller/game/arena/ArenaController.java
@@ -7,6 +7,7 @@ import com.ldts.donkeykong.gui.GUI;
 import com.ldts.donkeykong.model.game.arena.Arena;
 import com.ldts.donkeykong.model.game.elements.dynamic.Barrel;
 import com.ldts.donkeykong.model.menu.Menu;
+import com.ldts.donkeykong.states.MenuState;
 
 import java.io.IOException;
 
@@ -29,7 +30,7 @@ public class ArenaController extends GameController {
 
     public void step(Application application, GUI.ACTION action, long time) throws IOException {
         if (action == GUI.ACTION.QUIT || getModel().getMario().getScore() == 0) {
-
+            application.setState(new MenuState(new Menu()));
         }
         else {
             marioController.step(application, action, time);
