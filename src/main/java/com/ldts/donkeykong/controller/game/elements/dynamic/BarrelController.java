a110905e77d359ef421bd08af204cedd4d1ffe5f
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-18 15:06:15 +0000

    Added implementation for BarrelController

--- src/main/java/com/ldts/donkeykong/controller/game/elements/dynamic/BarrelController.java
+++ src/main/java/com/ldts/donkeykong/controller/game/elements/dynamic/BarrelController.java
@@ -3,6 +3,7 @@ package com.ldts.donkeykong.controller.game.elements.dynamic;
 import com.ldts.donkeykong.Application;
 import com.ldts.donkeykong.controller.game.GameController;
 import com.ldts.donkeykong.gui.GUI;
+import com.ldts.donkeykong.model.base.Position;
 import com.ldts.donkeykong.model.game.arena.Arena;
 import com.ldts.donkeykong.model.game.elements.dynamic.Barrel;
 
@@ -14,23 +15,41 @@ public class BarrelController extends GameController {
 
     public BarrelController(Arena arena) {
         super(arena);
+        direction = Direction.RIGHT;
     }
 
     private void moveBarrels() {
-
+        for(int i = 0; i < getModel().getBarrels().size(); i++) {
+            moveBarrel(getModel().getBarrels().get(i));
+        }
     }
 
     private void moveBarrel(Barrel barrel) {
+        if(!getModel().isInArena(barrel.getPosition())) getModel().removeBarrel(barrel);
 
+        if(barrel.getPosition().equals(new Position(getModel().getHeight()-1,getModel().getWidth()-1)))
+            getModel().removeBarrel(barrel);
+
+        else if(!getModel().isStructure(barrel.getPosition().getDown()) && getModel().isInArena(barrel.getPosition().getDown())) {
+
+            barrel.setPosition(barrel.getPosition().getDown());
+            if(!barrelFalling(barrel) && direction.equals(Direction.RIGHT)) direction = Direction.LEFT;
+            else if(!barrelFalling(barrel) && direction.equals(Direction.LEFT)) direction = Direction.RIGHT;
+
+        } else if(direction.equals(Direction.RIGHT)) {
+            barrel.setPosition(barrel.getPosition().getRight());
+        } else if (direction.equals(Direction.LEFT)) {
+            barrel.setPosition(barrel.getPosition().getLeft());
+        }
     }
 
     private boolean barrelFalling(Barrel barrel) {
-        return false;
+        return !getModel().isStructure(barrel.getPosition().getDown());
     }
 
     @Override
     public void step(Application application, GUI.ACTION action, long time) throws IOException {
-
+        moveBarrels();
     }
 
     enum Direction {LEFT,RIGHT}
